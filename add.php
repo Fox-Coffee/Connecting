d32959be3d8ca38ca216e1d1c633ae315cd44897
Foxiakowsky <foxiakkomunista@gmail.com>
2022-10-18 16:02:35 +0200

    Fixed

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
     require 'inc/load.php';
     require 'inc/config.php';
-    $pol = new Connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_BASENAME);
+    $pol = new Connect(DB_SOFTWARE, DB_SERVER, DB_USER, DB_PASSWORD, DB_BASENAME);
     $query = $pol->sql->prepare("INSERT INTO tabela(Imie, Nazwisko, Data_Urodzenia, Plec, Miasto_Zamieszkania) VALUES(?, ?, ?, ?, ?)");
     if(empty($_POST['name']) or empty($_POST['last']) or empty($_POST['date']) or empty($_POST['gender']) or empty($_POST['city'])){
         echo"<script>alert('Nie podano danych!');
--- classes/connect.class.php
+++ classes/connect.class.php
@@ -6,20 +6,20 @@ class Connect{
     private $password;
     private $basename;
     public $sql;
-    private function __construct($software, $server, $username, $password, $basename){
+    public function __construct($software, $server, $username, $password, $basename){
         try{
-            $this->$software = $software;
+            $this->software = $software;
             $this->server = $server;
             $this->username = $username;
             $this->password = $password;
             $this->basename = $basename;
-            $this->sql = new PDO($software.':host='.$this->server.";dbname=".$this->basename, $this->username, $this->password);
+            $this->sql = new PDO($this->software.':host='.$this->server.";dbname=".$this->basename, $this->username, $this->password);
             $this->sql->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $e){
-            header('Location: error.html');
+            //header('Location: error.html');
         }
     }
-    private function __destruct(){
+    public function __destruct(){
         $this->sql=null;
     }
 }
